60556b1806a94ddcb199124cb40b4d2a724e30ca
Alessandro Nada <anada000@login01.g100.cineca.it>
2024-10-01 15:44:35 +0200

    Changed compile script

--- script_YM_compilation.sh
+++ script_YM_compilation.sh
@@ -2,7 +2,8 @@
 
 NCOLORS=3
 NTHREADS=1
-compile_targets='yang_mills_local yang_mills_local_pt yang_mills_local_pt_multicanonic conf_check yang_mills_t0 yang_mills_gradient_flow yang_mills_local_jarzynski'
+compile_targets='yang_mills_local_jarzynski'
+#'yang_mills_local yang_mills_local_pt yang_mills_local_pt_multicanonic conf_check yang_mills_t0 yang_mills_gradient_flow yang_mills_local_jarzynski'
 
 # 1=yes, 0=no
 ENABLE_THETA=0
@@ -20,10 +21,10 @@ if [ -d lib/.deps ]; then make clean; fi
 #./configure N_c=${NCOLORS} Num_threads=${NTHREADS} CC=gcc CFLAGS='-O3' ${flag_openmp} ${flag_theta}
 
 # optimized configuration for Marconi (optimized compilation with Intel compiler on Intel Skylake processors)
-./configure N_c=${NCOLORS} Num_threads=${NTHREADS} CC=icc CFLAGS='-O3 -axCORE-AVX512 -mtune=skylake -ip -ipo' LIBS="-ldl -lz -lc" ${flag_openmp} ${flag_theta}
+#./configure N_c=${NCOLORS} Num_threads=${NTHREADS} CC=icc CFLAGS='-O3 -axCORE-AVX512 -mtune=skylake -ip -ipo' LIBS="-ldl -lz -lc" ${flag_openmp} ${flag_theta}
 
 # optimized configuration for Galileo100 (optimized compilation with Intel compiler on Intel Cascadelake processors)
-#./configure N_c=${NCOLORS} Num_threads=${NTHREADS} CC=icc CFLAGS='-O3 -axCORE-AVX512 -mtune=cascadelake -ip -ipo' LIBS="-ldl -lz -lc" ${flag_openmp} ${flag_theta}
+./configure N_c=${NCOLORS} Num_threads=${NTHREADS} CC=icc CFLAGS='-O3 -axCORE-AVX512 -mtune=cascadelake -ip -ipo' LIBS="-ldl -lz -lc" ${flag_openmp} ${flag_theta}
 
 # compile
 make ${compile_targets} # -j 18
